18191d948cc2e7ac5ae15b2057b76335ec6fb27d
Alan Denison <alan79td@gmail.com>
2016-07-15 09:56:23 -0700

    add GetAll and DeleteAll methods, pass ClientTest for making sure test database is empty

---
+++ Objects/Client.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace HairSalon
+{
+  public class Client
+  {
+    private int _id;
+    private string _name;
+    private int _stylistId;
+
+    public Client(string Name, int StylistId, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+      _stylistId = StylistId;
+    }
+
+    public int GetStylistId()
+    {
+      return _stylistId;
+    }
+    public void SetStylistId(int newStylistId)
+    {
+      _stylistId = newStylistId;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM clients;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string clientName = rdr.GetString(1);
+        int clientStylistId = rdr.GetInt32(2);
+        Client newClient = new Client(clientName, stylistId);
+        allClients.Add(newClient);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allClients;
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM clients;", conn);
+      cmd.ExecuteNonQuery();
+    }
+  }
+}
