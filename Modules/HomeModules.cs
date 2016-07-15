5895c8312f5998e7eab0bf1db5e4675d34321f95
Alan Denison <alan79td@gmail.com>
2016-07-15 14:26:06 -0700

    create Nancy app and views

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -12,6 +12,24 @@ namespace HairSalon.Objects
         List<Stylist> AllStylists = Stylist.GetAll();
         return View["index.cshtml", AllStylists];
       };
+      Post["/stylist/new"] = _ => {
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
+        newStylist.Save();
+        return View["success.cshtml"];
+      };
+      Get["/stylists/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var SelectedStylist = Stylist.Find(parameters.id);
+        var StylistClients = SelectedStylist.GetClients();
+        model.Add("stylist", SelectedStylist);
+        model.Add("clients", StylistClients);
+        return View["stylists.cshtml", model];
+      };
+      Post["/client/new"] = _ => {
+        Client newClient = new Client(Request.Form["client-name"], Request.Form["client-stylist"]);
+        newClient.Save();
+        return View["success.cshtml"];
+      };
     }
   }
 }
