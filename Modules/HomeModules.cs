ef7bc6386f2309d94b7d60f954cb4c442c471303
Alan Denison <alan79td@gmail.com>
2016-07-15 14:46:39 -0700

    add ability to edit stylists in Nancy app

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -30,6 +30,15 @@ namespace HairSalon.Objects
         newClient.Save();
         return View["success.cshtml"];
       };
+      Get["stylist/edit/{id}"] = parameters => {
+        Stylist SelectedStylist = Stylist.Find(parameters.id);
+        return View["stylist_edit.cshtml", SelectedStylist];
+      };
+      Patch["stylist/edit/{id}"] = parameters => {
+        Stylist SelectedStylist = Stylist.Find(parameters.id);
+        SelectedStylist.Update(Request.Form["stylist-name"]);
+        return View["success.cshtml"];
+      };
     }
   }
 }
